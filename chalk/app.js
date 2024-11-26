b0882330c93a65d755b9467ed2709702bd96ff22
saadhn4 <saadcfi4@gmail.com>
2024-11-26 12:15:08 +0530

    env

---
+++ chalk/app.js
@@ -0,0 +1,35 @@
+import chalk from "chalk";
+
+// console.log(chalk.black.bgCyan.bold("Hello, world!"));
+
+// console.log(chalk.bgMagenta("Text with Magenta background"));
+
+// let result = chalk.rgb(123, 45, 67).underline("Underline text");
+// console.log(result);
+
+// console.log(chalk.hidden.bgBlueBright("Hidden text"));
+
+console.log(chalk.green("**************************************************"));
+console.log(chalk.yellow("          🍅Welcome to Fruit shop :D🍉        "));
+console.log("                                                     ");
+console.log(chalk.green("**************************************************"));
+
+console.log(chalk.blue("---------------------------------------------------"));
+
+console.log(
+  chalk.redBright("     😊We are happy to serve you ‼️‼️‼️‼️‼️😊       ")
+);
+
+console.log(chalk.blue("---------------------------------------------------"));
+
+console.log(chalk.rgb(255, 153, 51)("YEAHHHHHHH THIS IS SAFFRON COLOR"));
+
+console.log(chalk.blue("---------------------------------------------------"));
+
+//INDIAN FLAG:
+
+console.log("Racist Indian Flag");
+
+console.log(chalk.bgRedBright("            "));
+console.log(chalk.bgWhite("     💩     "));
+console.log(chalk.bgGreen.hidden("            "));
